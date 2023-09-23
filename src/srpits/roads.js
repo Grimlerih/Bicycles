007a4dceafc7e64d25611984812fae4f00f62b43
Grimlerih <kanatuulud@gmail.com>
2023-09-23 17:27:44 +0600

    fix: логика работы секции roads

--- src/srpits/roads.js
+++ src/srpits/roads.js
@@ -2,27 +2,25 @@ const leftBtn = document.querySelector(".roads__btns_left");
 const rightBtn = document.querySelector(".roads__btns_right");
 const container = Array.from(document.querySelector(".roads__img").children);
 const roadsInfo = Array.from(document.querySelectorAll(".roads__box"));
+const roadsVectorImg = Array.from(
+  document.querySelector(".roads__vector").children
+);
 
 const scrollAmount = 700;
 var currentIndex = 0;
 
 const swtchInfo = (operator) => {
-  if (roadsInfo[currentIndex]) {
-    roadsInfo[currentIndex].classList.add("hidden");
-  }
+  roadsInfo[currentIndex].classList.add("hidden");
+  roadsVectorImg[currentIndex].classList.add("hidden");
 
   if (operator === "plus") {
-    currentIndex++;
+    currentIndex = (currentIndex + 1) % roadsInfo.length;
   } else if (operator === "minus") {
-    currentIndex--;
+    currentIndex = (currentIndex - 1 + roadsInfo.length) % roadsInfo.length;
   }
-  console.log(currentIndex);
-  if (roadsInfo[currentIndex] && currentIndex !== 3) {
-    roadsInfo[currentIndex].classList.remove("hidden");
-  } else {
-    currentIndex = 0;
-    roadsInfo[0].classList.remove("hidden");
-  }
+
+  roadsInfo[currentIndex].classList.remove("hidden");
+  roadsVectorImg[currentIndex].classList.remove("hidden");
 };
 
 const updateCarousel = () => {
